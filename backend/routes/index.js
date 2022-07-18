169ee3a53c3f6fe988266cc62ba7a70eebad428b
Theo Fandrich <thelogfan42@gmail.com>
2022-07-18 12:18:54 -0700

    Created Front Page

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,6 +3,10 @@ const express = require("express");
 const router = express.Router();
 const apiRouter = require("./api");
 
+router.get("/", (req, res) => {
+  res.send("Welcome to Airbnb");
+});
+
 router.use("/api", apiRouter);
 
 module.exports = router;
