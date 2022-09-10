35e184baafe347d67023a3e2255efd701f08842c
Theo Fandrich <thelogfan42@gmail.com>
2022-09-10 16:12:25 -0700

    hi

--- backend/db/seeders/20220720031139-demo-spots.js
+++ backend/db/seeders/20220720031139-demo-spots.js
@@ -2,53 +2,57 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.bulkInsert("Spots", [
-      {
-        ownerId: 1,
-        address: "111 Adam St.",
-        city: "Dallas",
-        state: "TX",
-        country: "USA",
-        lat: 38.8951,
-        lng: -77.0364,
-        name: "Homey Home for the Homies",
-        description:
-          "A home once enjoyed by the homies, now available to the homies,",
-        price: 50.93,
-        previewImage:
-          "https://airbnb-project.s3.us-west-2.amazonaws.com/Spot+1/0425a5a1-394e-4371-8bf8-506016d39fd6.webp",
-      },
-      {
-        ownerId: 2,
-        address: "222 Bill St.",
-        city: "Dallas",
-        state: "TX",
-        country: "USA",
-        lat: 41.8951,
-        lng: -79.0364,
-        name: "2nd Homey Home for the Homies",
-        description:
-          "Also a home once enjoyed by the homies, now available to the homies,",
-        price: 55.93,
-        previewImage:
-          "https://airbnb-project.s3.us-west-2.amazonaws.com/Spot+1/0425a5a1-394e-4371-8bf8-506016d39fd6.webp",
-      },
-      {
-        ownerId: 3,
-        address: "333 Carter St.",
-        city: "Dallas",
-        state: "TX",
-        country: "USA",
-        lat: 51.8251,
-        lng: -29.0364,
-        name: "3rd Homey Home for the Homies",
-        description:
-          "3rd home once enjoyed by the homies, now available to the homies,",
-        price: 25.93,
-        previewImage:
-          "https://airbnb-project.s3.us-west-2.amazonaws.com/Spot+1/0425a5a1-394e-4371-8bf8-506016d39fd6.webp",
-      },
-    ]);
+    try {
+      await queryInterface.bulkInsert("Spots", [
+        {
+          ownerId: 1,
+          address: "111 Adam St.",
+          city: "Dallas",
+          state: "TX",
+          country: "USA",
+          lat: 38.8951,
+          lng: -77.0364,
+          name: "Homey Home for the Homies",
+          description:
+            "A home once enjoyed by the homies, now available to the homies.",
+          price: 50.93,
+          previewImage:
+            "https://airbnb-project.s3.us-west-2.amazonaws.com/Spot+1/0425a5a1-394e-4371-8bf8-506016d39fd6.webp",
+        },
+        {
+          ownerId: 2,
+          address: "222 Bill St.",
+          city: "Dallas",
+          state: "TX",
+          country: "USA",
+          lat: 41.8951,
+          lng: -79.0364,
+          name: "2nd Homey Home for the Homies",
+          description:
+            "Also a home once enjoyed by the homies, now available to the homies.",
+          price: 55.93,
+          previewImage:
+            "https://airbnb-project.s3.us-west-2.amazonaws.com/Spot+1/0425a5a1-394e-4371-8bf8-506016d39fd6.webp",
+        },
+        {
+          ownerId: 3,
+          address: "333 Carter St.",
+          city: "Dallas",
+          state: "TX",
+          country: "USA",
+          lat: 51.8251,
+          lng: -29.0364,
+          name: "3rd Homey Home for the Homies",
+          description:
+            "3rd home once enjoyed by the homies, now available to the homies.",
+          price: 25.93,
+          previewImage:
+            "https://airbnb-project.s3.us-west-2.amazonaws.com/Spot+1/0425a5a1-394e-4371-8bf8-506016d39fd6.webp",
+        },
+      ]);
+    } catch (e) {
+      console.log(e);
+    }
   },
 
   async down(queryInterface, Sequelize) {
